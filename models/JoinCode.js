0a128d4944286a6393418631ec0d57991b344d4e
Kelvin Ohaya <kelvinohaya@gmail.com>
2025-09-03 18:52:27 -0400

    restored files

--- models/JoinCode.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const { Schema } = mongoose
-
-const joinCodeSchema = new Schema({
-    content: {type: String, required: true},
-}, {timestamps: true})
-
-module.exports = mongoose.model("JoinCode", joinCodeSchema)
